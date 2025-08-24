638c1b57a8f7e39e419c7117eabc04052fb8e721
Fabio Balbino <fbribeiro@live.com>
2025-08-23 22:53:02 -0300

    feat: categories

---
+++ expense_tracker_app/lib/domain/enums/category.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+enum Category {
+  food,
+  transport,
+  entertainment,
+  home,
+  company,
+  health,
+  personal,
+  other,
+}
+
+extension CategoryExtension on Category {
+  IconData get icon {
+    switch (this) {
+      case Category.food:
+        return Icons.restaurant;
+      case Category.transport:
+        return Icons.directions_car;
+      case Category.entertainment:
+        return Icons.movie;
+      case Category.home:
+        return Icons.home;
+      case Category.company:
+        return Icons.business;
+      case Category.health:
+        return Icons.local_hospital;
+      case Category.personal:
+        return Icons.person;
+      case Category.other:
+        return Icons.category;
+    }
+  }
+
+  Color get color {
+    switch (this) {
+      case Category.food:
+        return Colors.orange;
+      case Category.transport:
+        return Colors.blue;
+      case Category.entertainment:
+        return Colors.purple;
+      case Category.home:
+        return Colors.green;
+      case Category.company:
+        return Colors.teal;
+      case Category.health:
+        return Colors.red;
+      case Category.personal:
+        return Colors.indigo;
+      case Category.other:
+        return Colors.grey;
+    }
+  }
+}
