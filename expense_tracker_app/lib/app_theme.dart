38cf12e8b369e983c02cbd03676ef62be4f37f81
Fabio Balbino <fbribeiro@live.com>
2025-08-23 23:34:33 -0300

    chore: app theme customizations

--- expense_tracker_app/lib/app_theme.dart
+++ expense_tracker_app/lib/app_theme.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
+const kPrimaryColor = Color(0xFF046B4D);
+const kSecondaryColor = Color(0xFF35464E);
+
 final ThemeData appTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  colorScheme: ColorScheme(
+    brightness: Brightness.light,
+    primary: kPrimaryColor,
+    onPrimary: Colors.white,
+    secondary: kSecondaryColor,
+    onSecondary: Colors.white,
+    error: Colors.red,
+    onError: Colors.white,
+    surface: Colors.white,
+    onSurface: Colors.black,
+  ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
+      backgroundColor: kPrimaryColor,
+      foregroundColor: Colors.white,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
       minimumSize: const Size(100, 60),
     ),
   ),
   outlinedButtonTheme: OutlinedButtonThemeData(
     style: OutlinedButton.styleFrom(
+      foregroundColor: kPrimaryColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
       minimumSize: const Size(140, 60),
     ),
