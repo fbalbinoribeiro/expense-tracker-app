ee96dad34018de1e4560fa5a8322aa261ff346c4
Fabio Balbino <fbribeiro@live.com>
2025-08-23 23:25:15 -0300

    chore: new theme file

---
+++ expense_tracker_app/lib/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      minimumSize: const Size(100, 60),
+    ),
+  ),
+  outlinedButtonTheme: OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(6)),
+      minimumSize: const Size(140, 60),
+    ),
+  ),
+);
