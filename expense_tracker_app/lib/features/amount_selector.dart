d810e70c65a1ea97203b32c4782de5e85c4d6fba
Fabio Balbino <fbribeiro@live.com>
2025-08-23 23:42:03 -0300

    feat: make the quick actions buttons to work

--- expense_tracker_app/lib/features/amount_selector.dart
+++ expense_tracker_app/lib/features/amount_selector.dart
@@ -1,12 +1,31 @@
 import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
 import 'package:flutter/material.dart';
 
-class AmountSelector extends StatelessWidget {
+class AmountSelector extends StatefulWidget {
   const AmountSelector({super.key});
 
   @override
+  State<AmountSelector> createState() => _AmountSelectorState();
+}
+
+class _AmountSelectorState extends State<AmountSelector> {
+  final TextEditingController _controller = TextEditingController();
+  final quickAmounts = [1, 2, 5, 10, 20, 50, 100, 200, 500];
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _addAmount(int amount) {
+    final current = int.tryParse(_controller.text) ?? 0;
+    final newValue = current + amount;
+    _controller.text = newValue.toString();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final quickAmounts = [1, 2, 5, 10, 20, 50, 100, 200, 500];
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -14,8 +33,9 @@ class AmountSelector extends StatelessWidget {
         children: [
           const SizedBox(height: 16),
           SizedBox(
-            width: double.infinity,
+            width: 300,
             child: TextField(
+              controller: _controller,
               decoration: InputDecoration(border: OutlineInputBorder()),
               keyboardType: TextInputType.numberWithOptions(decimal: true),
               inputFormatters: [
@@ -36,7 +56,7 @@ class AmountSelector extends StatelessWidget {
                 width: 100,
                 height: 60,
                 child: ElevatedButton(
-                  onPressed: null, // No interaction for now
+                  onPressed: () => _addAmount(amount),
                   child: Text(amount.toString()),
                 ),
               );
@@ -52,7 +72,7 @@ class AmountSelector extends StatelessWidget {
                   width: 140,
                   height: 60,
                   child: OutlinedButton(
-                    onPressed: null, // No interaction for now
+                    onPressed: () => _controller.clear(),
                     child: const Text('Clear', style: TextStyle(fontSize: 20)),
                   ),
                 ),
